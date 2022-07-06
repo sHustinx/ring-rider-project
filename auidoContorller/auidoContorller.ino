4b09853d4053fe580f0951be4782a75399fb7151
LuizFelipe13579 <107492753+LuizFelipe13579@users.noreply.github.com>
2022-07-06 16:14:14 +0200

    Add files via upload

--- auidoContorller/auidoContorller.ino
+++ auidoContorller/auidoContorller.ino
@@ -6,13 +6,10 @@
 SdReader card;    // This object holds the information for the card
 FatVolume vol;    // This holds the information for the partition on the card
 FatReader root;   // This holds the information for the volumes root directory
-FatReader file;   // This object represent the WAV file 
+FatReader file;   // This object represent the WAV file
 WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
 
-#define PLAY_TIME 300
-
-uint16_t fileIndex[90];
-char fileLetter[] =  {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; 
+const String fileformat = ".wav";
 
 //////////////////////////////////// SETUP
 void setup() {
@@ -22,56 +19,31 @@ void setup() {
   vol.init(card);
   root.openRoot(vol);
 
-  indexFiles(); 
+  Serial.begin(115200);
 
   Wire.begin(0x67);                // join I2C bus with address 0x67
-  Wire.onReceive(receiveEvent); // register event 
+  Wire.onReceive(receiveEvent); // register event
 }
 
 void loop() {
 }
 
-void indexFiles(void) {
-  char name[7];
-  
-  // copy flash string to RAM
-  strcpy_P(name, PSTR("xy.wav"));
-  
-  for (uint8_t i = 0; i < 9; i++) {
-    for (uint8_t j = 1; j < 10; j++) {     
-      // Make file name
-      name[0] = fileLetter[i];
-      name[1] = fileLetter[j];
-      // Open file by name
-      file.open(root, name);
-      // Save file's index (byte offset of directory entry divided by entry size)
-      // Current position is just after entry so subtract one.
-      fileIndex[(10*i)+j] = root.readPosition()/32 - 1;   
-    }
-  }
-}
 
-void playByIndex(int index) {
-  // start time
-  uint32_t t = millis();
-  
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()); // loop through all but the last
+  int recieved = Wire.read();
+
+  String filename = String(recieved) + fileformat;
+  char toopen[7];
+  filename.toCharArray(toopen, 7);
+  Serial.println(toopen);
   // open by index
-  file.open(root, fileIndex[index]);
-  
+  file.open(root, toopen);
+
   // create and play Wave
   wave.create(file);
   wave.play();
 
-  // stop after PLAY_TIME ms
-  while((millis() - t) < PLAY_TIME);
-  wave.stop();
-}
-
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
-void receiveEvent(int howMany) {    
-playByIndex(Wire.read());        // receive byte as an integer
-Wire.beginTransmission(0x66);    // transmit to device #0x66
-Wire.write('y');                 // sends one byte
-Wire.endTransmission();          // stop transmitting
 }
