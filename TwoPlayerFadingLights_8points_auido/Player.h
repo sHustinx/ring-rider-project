5ed15d5accfac255b896a8a764441d214e743647
LuizFelipe13579 <107492753+LuizFelipe13579@users.noreply.github.com>
2022-07-06 09:59:44 +0200

    Add files via upload

---
+++ TwoPlayerFadingLights_8points_auido/Player.h
@@ -0,0 +1,26 @@
+// Player.h
+#ifndef Player_h
+#define Player_h
+
+#include <Arduino.h>
+#include "GY521.h"
+
+class Player
+{
+private:
+  GY521 sensor;
+  double pitchR, rollR, planeX, planeY, projectAng;
+  bool forward_F, forward_h, backward_F, backward_h, left_F, left_h, right_F, right_h, xNull, yNull, isCPU, is_match;
+  float THRESHOLD;
+  byte prev_pos, current_pos, combo_length;
+  void check_combo(byte c_pos, byte p_pos, long c_millis, long match_duration);
+  long match_time;
+
+public:
+  Player(int address, float th, float ax, float ay, float az, float gx, float gy, float gz, byte combo_len);
+  bool is_combo_mode;
+  void wakeUp_n_check();
+  byte currentPos(), combo[];
+};
+
+#endif
